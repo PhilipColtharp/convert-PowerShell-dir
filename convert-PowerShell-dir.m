94030565f41f55ff9d421170c5e6890c3e415776
PhilipColtharp <coltharp.philip@gmail.com>
2022-08-31 16:45:56 -0400

    Update convert-PowerShell-dir.m

--- convert-PowerShell-dir.m
+++ convert-PowerShell-dir.m
@@ -1,25 +1,31 @@
 // PowerShell command:
 //     cd "To the folder to start at"
 //     Get-ChildItem "whatever*wildcard" -Force -Recurse  | Format-Table -AutoSize | Out-File A:\OutputHere.txt -width 300
-
 let
     Source = Table.FromColumns(
-      {Lines.FromBinary(File.Contents("dir file.txt"), null, null, 1252)},
+      {Lines.FromBinary(File.Contents("C:\Users\plc320\OneDrive - Hanover Insurance\Desktop\Clay Monthly\ClayDir2.txt"), null, null, 1252)},
       {"txt"}
       ),
-    #"Filtered Rows" = Table.SelectRows(#"Source",
-        each (not Text.Contains([txt], "Mode                 LastWriteTime         Length Name ") 
-          and not Text.Contains([txt], "----                 -------------         ------ ---- ") 
+    #"Filtered Rows" = Table.SelectRows(Source,
+        each (
+          not Text.StartsWith([txt], "Mode ")
+          and not Text.StartsWith([txt], "---- ") 
           and  [txt]<>null
           and  [txt]<>""
         )
     ),
-    #"Added Conditional Column" = Table.AddColumn(#"Filtered Rows", "Folder", 
+    //Folder Column derived from Directory lines
+    #"Add Folder Column" = Table.AddColumn(#"Filtered Rows", "Folder", 
       each if Text.StartsWith([txt], "    Directory:") then Text.Middle([txt],15) else null),
-    #"Filled Down" = Table.FillDown(#"Added Conditional Column",{"Folder"}),
-    #"Filtered Rows1" = Table.SelectRows(#"Filled Down", each not Text.StartsWith([txt], "    Directory:")),
+    #"Fill Down" = Table.FillDown(#"Add Folder Column",{"Folder"}),
+    #"Filtered Rows1" = Table.SelectRows(#"Fill Down", each not Text.StartsWith([txt], "    Directory:")),
     #"Split Column by Position" = Table.SplitColumn(#"Filtered Rows1", "txt", 
-      Splitter.SplitTextByPositions({0, 6,34,50}, false), {"txt.1", "txt.2", "txt.3", "txt.4"}),
-    #"Changed Type" = Table.TransformColumnTypes(#"Split Column by Position",{{"txt.1", type text}, {"txt.2", type datetime}, {"txt.3", Int64.Type}, {"txt.4", type text}})
+      Splitter.SplitTextByPositions({0, 6,34,50}, false), {"Mode", "LastWriteTime", "Length", "Name"}),
+    #"Changed Type" = Table.TransformColumnTypes(#"Split Column by Position",{
+        {"Mode", type text}, 
+        {"LastWriteTime", type datetime}, 
+        {"Length", Int64.Type}, 
+        {"Name", type text}
+        })
 in
     #"Changed Type"
