f89e65d9ef8ec4cc3914bb204ef5d37cd75ef886
PhilipColtharp <coltharp.philip@gmail.com>
2022-08-31 16:46:14 -0400

    Update convert-PowerShell-dir.m

--- convert-PowerShell-dir.m
+++ convert-PowerShell-dir.m
@@ -1,31 +1,73 @@
+let
+    Source = Table.FromColumns(
+      {Lines.FromBinary(File.Contents(#"File Location"), null, null, 1252)},
+      {"txt"}
+      ),
+    #"Separations" = 
+      Table.AddColumn(
+        Source, 
+        "Separations", 
+        each if Text.StartsWith([txt], "Mode") 
+          then  {0,
+                 8,
+                 Text.PositionOf([txt],"LastWriteTime")+14,
+                 Text.PositionOf([txt],"Name")} 
+          else null),
 // PowerShell command:
 //     cd "To the folder to start at"
 //     Get-ChildItem "whatever*wildcard" -Force -Recurse  | Format-Table -AutoSize | Out-File A:\OutputHere.txt -width 300
-let
-    Source = Table.FromColumns(
-      {Lines.FromBinary(File.Contents("C:\Users\plc320\OneDrive - Hanover Insurance\Desktop\Clay Monthly\ClayDir2.txt"), null, null, 1252)},
-      {"txt"}
+    #"Folders" = 
+      Table.AddColumn(
+        #"Separations", 
+        "Folder", 
+        each if Text.StartsWith([txt], "    Directory:") 
+          then Text.Middle([txt],15) 
+          else null
+        ),
+    #"Fill Down" = Table.FillDown(#"Folders",{"Folder","Separations"}),
+    #"Delete Rows" = 
+      Table.SelectRows(
+        #"Fill Down",
+          each (not Text.StartsWith([txt], "Mode") 
+            and not Text.StartsWith([txt], "----")
+            and not Text.StartsWith([txt], "    Directory:") 
+            and  [txt]<>null
+            and  [txt]<>""
+            )
       ),
-    #"Filtered Rows" = Table.SelectRows(Source,
-        each (
-          not Text.StartsWith([txt], "Mode ")
-          and not Text.StartsWith([txt], "---- ") 
-          and  [txt]<>null
-          and  [txt]<>""
+    #"Insert delimiters" = 
+      Table.AddColumn(
+        #"Delete Rows", 
+        "NewColumns", 
+        each Text.Combine(
+          Splitter.SplitTextByPositions([Separations])([txt]),
+          "\"
         )
-    ),
-    //Folder Column derived from Directory lines
-    #"Add Folder Column" = Table.AddColumn(#"Filtered Rows", "Folder", 
-      each if Text.StartsWith([txt], "    Directory:") then Text.Middle([txt],15) else null),
-    #"Fill Down" = Table.FillDown(#"Add Folder Column",{"Folder"}),
-    #"Filtered Rows1" = Table.SelectRows(#"Fill Down", each not Text.StartsWith([txt], "    Directory:")),
-    #"Split Column by Position" = Table.SplitColumn(#"Filtered Rows1", "txt", 
-      Splitter.SplitTextByPositions({0, 6,34,50}, false), {"Mode", "LastWriteTime", "Length", "Name"}),
-    #"Changed Type" = Table.TransformColumnTypes(#"Split Column by Position",{
-        {"Mode", type text}, 
-        {"LastWriteTime", type datetime}, 
-        {"Length", Int64.Type}, 
-        {"Name", type text}
-        })
+        ),
+    #"Split Column by Delimiter" = 
+      Table.SplitColumn(
+        #"Insert delimiters", 
+        "NewColumns", 
+        Splitter.SplitTextByDelimiter("\", QuoteStyle.None),
+        {"Mode", "LastWriteTime", "Length", "Name"}
+        ),
+    #"Added ext" = 
+      Table.AddColumn(
+        #"Split Column by Delimiter", 
+        "ext", 
+        each if Text.Contains([Name], ".") 
+          then Text.AfterDelimiter([Name], ".", {0, RelativePosition.FromEnd}) 
+          else ""
+          ),
+    #"Remove Columns" = Table.RemoveColumns(#"Added ext",{"txt", "Separations"}),
+    #"Changed Type" = 
+      Table.TransformColumnTypes(
+          #"Remove Columns",
+          { {"Mode", type text}, 
+            {"LastWriteTime", type datetime}, 
+            {"Length", Int64.Type}, 
+            {"Name", type text}
+          }
+      )
 in
-    #"Changed Type"
+    #"Remove Columns"
